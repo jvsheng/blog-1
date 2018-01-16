bc949aaad8516811f906b20c67f7b812caa0621c
jvsheng <huangjvsheng@hotmail.com>
2018-01-16 22:42:10 +0800

    update offsetParent example

--- utils.js
+++ utils.js
@@ -1,1 +1,10 @@
-
+/**
+ * 1. 返回最近的包含该元素的定位元素
+ * @param element
+ * @returns {Element} offset parent
+ * @example https://jsfiddle.net/Viajes/br6uw9vj/3/
+ */
+function getOffsetParent(element) {
+  var offsetParent = element.offsetParent;
+  return offsetParent === window.document.body || !offsetParent ? window.document.documentElement : offsetParent;
+}
